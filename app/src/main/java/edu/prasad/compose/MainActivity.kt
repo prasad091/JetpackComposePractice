f0c1a9c7c5275498c899961d2fe3f0c5f9ffdc5f
prasad091 <prasad.thangavel@gmail.com>
2022-07-21 15:57:59 +0530

    Feat: Create compose page and display the canvas

---
+++ app/src/main/java/edu/prasad/compose/MainActivity.kt
@@ -0,0 +1,103 @@
+package edu.prasad.compose
+
+import android.os.Bundle
+import android.text.Selection
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.primarySurface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.documentfile.provider.DocumentFile
+import edu.prasad.compose.ui.theme.ComposeTheme
+import java.util.*
+import kotlin.math.min
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+
+    Column {
+        Text(text = "Hello ${onLoadChnage()}---!")
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            val radius = min(size.width,size.height) / 2.0f
+            val padding = 20
+            drawCircle(color = Color.Black, radius = radius - padding, style = Stroke(width = 5.0f))
+            val path = Path().apply {
+                moveTo(padding.toFloat(),center.y)
+                lineTo(x=size.width - padding,y=center.y)
+            }
+            drawPath(
+                path = path,
+                color = Color.Red,
+                style = Stroke(width = 5.0f)
+            )
+        }
+
+    }
+}
+
+@Composable
+fun onLoadChnage():String{
+    return "true"
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeTheme {
+        Greeting("Android")
+    }
+}
+
+@Composable
+fun NameInput() {
+    val textValue = remember{ mutableStateOf("")}
+    textValue.value = "Hello"
+}
+
+@Composable
+fun BeautifulImage() {
+    Image(
+        painter =
+        painterResource(R.drawable.ic_launcher_foreground),
+        contentDescription = "My app icon",
+        contentScale = ContentScale.Fit
+    )
+}
+
